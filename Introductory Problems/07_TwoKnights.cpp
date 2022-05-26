7706b5e0a63a431c8415061c99fa7e64c2c2f86d
SahilRaj1 <raj24sahil@gmail.com>
2022-05-26 23:41:02 +0530

    commit

---
+++ Introductory Problems/07_TwoKnights.cpp
@@ -0,0 +1,22 @@
+// Two Knights
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // Code here
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << ((i * i) * (i * i - 1) / 2) - (4 * (i - 2) * (i - 1)) << endl;
+    }
+
+    return 0;
+}
