77992c3da456d35f743c0f21ce4ae0a39386c624
SahilRaj1 <raj24sahil@gmail.com>
2022-05-26 23:42:32 +0530

    commit

--- Introductory Problems/07_TwoKnights.cpp
+++ Introductory Problems/07_TwoKnights.cpp
@@ -11,9 +11,9 @@ int main()
 #endif
 
     // Code here
-    int n;
+    long long n;
     cin >> n;
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
         cout << ((i * i) * (i * i - 1) / 2) - (4 * (i - 2) * (i - 1)) << endl;
     }
