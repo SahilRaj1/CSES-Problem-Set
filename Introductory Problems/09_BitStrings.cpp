0c4f0c944bfaf53d7bbf7b5394dd40e293227c91
SahilRaj1 <raj24sahil@gmail.com>
2022-05-31 19:21:01 +0530

    commit

---
+++ Introductory Problems/09_BitStrings.cpp
@@ -0,0 +1,21 @@
+// Bit Strings
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9 + 7;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // Code here
+    ll n;
+    cin >> n;
+    cout << (ll)(pow(2, n)) % mod << endl;
+
+    return 0;
+}
