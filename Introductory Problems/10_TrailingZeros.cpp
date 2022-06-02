b4e00cbaccafe8c60fd3634a0deaffa241722d85
SahilRaj1 <raj24sahil@gmail.com>
2022-06-02 16:01:20 +0530

    commit

--- Introductory Problems/10_TrailingZeros.cpp
+++ Introductory Problems/10_TrailingZeros.cpp
@@ -12,23 +12,12 @@ int main()
 #endif
 
     // Code here
-    int n, ct = 0, mul2 = 0;
+    int n, ct = 0;
     cin >> n;
-    for (int i = 1; i <= n; i++)
+
+    for (int i = 5; n / i >= 1; i *= 5)
     {
-        if (mul2 == 0 && i % 2 == 0)
-        {
-            mul2 = 2;
-        }
-        if (i % 10 == 0)
-        {
-            
-        }
-        else if (i % 5 == 0 && mul2 == 2)
-        {
-            ct++;
-            mul2 = 0;
-        }
+        ct += n / i;
     }
 
     cout << ct << endl;
