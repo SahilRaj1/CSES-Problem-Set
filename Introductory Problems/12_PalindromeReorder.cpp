61244427c68bcf5fefbe82e1fd4b26359c30726c
SahilRaj1 <raj24sahil@gmail.com>
2022-06-04 21:01:24 +0530

    commit

--- Introductory Problems/12_PalindromeReorder.cpp
+++ Introductory Problems/12_PalindromeReorder.cpp
@@ -16,7 +16,55 @@ int main()
     cout.tie(0);
 
     // Code here
-    
+    string s, s1 = "", s2 = "";
+    cin >> s;
+    int n = s.length();
+    map<char, int> m1, m2;
+    for (auto &it : s)
+    {
+        m1[it]++;
+        m2[it]++;
+    }
+
+    // cout << m2.size() << endl;
+
+    for (auto &item : m1)
+    {
+        char letter = item.first;
+        int ct = item.second;
+        auto it = m2.find(letter);
+        if (ct % 2 == 0)
+        {
+            for (int j = 0; j < (ct / 2); j++)
+            {
+                s1.push_back(letter);
+                s2.push_back(letter);
+            }
+            m2.erase(it);
+        }
+    }
+    // cout << m2.size() << endl;
+
+    reverse(s2.begin(), s2.end());
+
+    if (m2.size() > 1)
+    {
+        cout << "NO SOLUTION" << endl;
+    }
+    else if (m2.size() == 0)
+    {
+        cout << s1 << s2 << endl;
+    }
+    else
+    {
+        cout << s1;
+        pair<char, int> p = *m2.begin();
+        for (int i = 0; i < p.second; ++i)
+        {
+            cout << p.first;
+        }
+        cout << s2;
+    }
 
     return 0;
 }
