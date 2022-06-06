389ac928449591ec6fb4b09d819914a0a95822a0
SahilRaj1 <raj24sahil@gmail.com>
2022-06-06 23:25:51 +0530

    commit

--- Sorting and Searching/03_FerrisWheel.cpp
+++ Sorting and Searching/03_FerrisWheel.cpp
@@ -1,2 +1,71 @@
 // Ferris Wheel
 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<bool> vb;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
+#define fre(a, b) for (auto &(a) : (b))
+#define fr(i, n) FOR(i, 0, n)
+#define frn(i, n) FORN(i, 1, n)
+#define sqr(x) ((LL)(x) * (x))
+#define endl "\n"
+#define pb push_back
+#define pp pop_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define alla(arr, sz) arr, arr + sz
+const int MOD = 1e9 + 7;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Code here
+    int n, x;
+    cin >> n >> x;
+    vi weights(n);
+    fr(i, n) cin >> weights[i];
+
+    sort(all(weights));
+    int i = 0, j = n - 1;
+    int ans = 0;
+    while (i <= j)
+    {
+        if (weights[i] + weights[j] <= x)
+        {
+            ans++;
+            i++;
+            j--;
+        }
+        else
+        {
+            ans++;
+            j--;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
