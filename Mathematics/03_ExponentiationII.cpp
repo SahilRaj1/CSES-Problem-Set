70372d35c22b711ac29af327fe1153acdc4ecb4c
SahilRaj1 <raj24sahil@gmail.com>
2022-06-07 14:34:50 +0530

    commit

---
+++ Mathematics/03_ExponentiationII.cpp
@@ -0,0 +1,76 @@
+// Exponentiation II
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<bool> vb;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
+#define fre(a, b) for (auto &(a) : (b))
+#define fr(i, n) FOR(i, 0, n)
+#define frn(i, n) FORN(i, 1, n)
+#define sqr(x) ((LL)(x) * (x))
+#define endl "\n"
+#define pb push_back
+#define pp pop_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define alla(arr, sz) arr, arr + sz
+const int MOD = 1e9 + 7;
+
+ll binExp(int a, int b, int M)
+{
+    ll ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans = (ans * 1LL * a) % M;
+        a = (a * 1LL * a) % M;
+        b = b >> 1;
+    }
+    return ans;
+}
+
+void solve()
+{
+    // Code here
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << binExp(a, binExp(b, c, MOD - 1), MOD) << endl;
+
+    return;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
